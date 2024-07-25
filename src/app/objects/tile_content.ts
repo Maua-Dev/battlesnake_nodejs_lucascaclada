49c0289a0deb6967c32b74c78929e3268046555a
LucasCalcada <lucasmgmcalcada@gmail.com>
2024-07-25 17:32:23 -0300

    add tile content class

---
+++ src/app/objects/tile_content.ts
@@ -0,0 +1,38 @@
+// Base class
+export class TileContent{
+  name:string
+  icon:string = '.';
+  constructor(tileName:string){
+    this.name = tileName;
+  }
+}
+
+// Enemy Snake
+export class SnakeBody extends TileContent{
+  size:number;
+  constructor(size:number){ super('Snake');
+    this.size = size;
+    this.icon = 'S';
+  }
+}
+
+export class SnakeTail extends SnakeBody{
+  constructor(size:number){
+    super(size);
+    this.icon = 'T';
+  }
+}
+export class SnakeHead extends SnakeBody{
+  constructor(size:number){
+    super(size);
+    this.icon = 'H';
+  }
+}
+
+// Food
+export class Food extends TileContent{
+  constructor(){
+    super('Food');
+    this.icon = 'F'
+  }
+}
