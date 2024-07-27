797584cb3b78e5fa8bc85c96a62c366b5e45467a
Lucas Calçada <lucasmgmcalcada@gmail.com>
2024-07-26 22:33:30 -0300

    change tile danger calculation

--- src/app/objects/tile.ts
+++ src/app/objects/tile.ts
@@ -11,22 +11,41 @@ export enum TileType{
   PlayerTail
 }
 
+export class Dangers{
+  snakeBody:boolean = false;
+  nearHead:boolean = false;
+  smallSet:boolean = false;
+}
+
 export class Tile{
   xPos:number;
   yPos:number;
   sidesKeys:string[] = [];
 
   tileType:TileType = TileType.Empty;
-  danger:number = 0;
+  
+  dangerStats:Dangers = new Dangers();
   reward:number = 0;
+
+  set:string;
+  setSize:number = 1;
 
   constructor(x:number, y:number, width:number = 11, height:number = 11){
     this.xPos = x;
     this.yPos = y;
+    this.set = (x + y * width).toString();
 
     if(this.xPos > 0) this.sidesKeys.push(Board.getTileKey(x - 1 , y));
     if(this.xPos < width - 1) this.sidesKeys.push(Board.getTileKey(x + 1, y));
     if(this.yPos > 0) this.sidesKeys.push(Board.getTileKey(x, y - 1));
     if(this.yPos < height - 2) this.sidesKeys.push(Board.getTileKey(x, y + 1));
+  }
+
+  get danger(){
+    let sum = 0;
+    for(let b in this.dangerStats){
+      if(b) sum += .1;
+    }
+    return sum;
   }
 }
